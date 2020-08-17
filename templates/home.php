70da260a852a459f953575fc67b03c1426d006b4
Zeller <matheo.zeller@gmail.com>
2020-08-17 17:34:19 +0200

    refactor(autoload): replace autoload for composer autoload

--- templates/home.php
+++ templates/home.php
@@ -3,11 +3,7 @@
 //On inclut le fichier dont on a besoin (ici à la racine de notre site)
 require '../src/DAO/DAO.php';
 
-require '../config/Autoloader.php';
-
-use \App\config\Autoloader;
-
-Autoloader::register(); ?>
+?>
 
 <h1>Mon blog</h1>
 <p>En construction</p>
