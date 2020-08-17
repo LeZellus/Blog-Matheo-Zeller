4a330b3791b16d59c47561dd9f4cf4ad1126a7be
Zeller <matheo.zeller@gmail.com>
2020-08-17 17:49:08 +0200

    feat(controller): add frontal controller

--- public/index.php
+++ public/index.php
@@ -1,12 +1,12 @@
 <?php
 
+require '../vendor/autoload.php';
 
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
-session_start();
-// $router = new \App\config\Router();
-// $router->run();
-
-return $this->view->render('index');
+try {
+    if (isset($_GET['route'])) {
+    } else {
+        require '../templates/home.php';
+    }
+} catch (Exception $e) {
+    echo 'Erreur';
+}
