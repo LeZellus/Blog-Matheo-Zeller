c32f406a79b2f5cb00f33d8b953d7f86665e25ee
Zeller <matheo.zeller@gmail.com>
2020-08-17 17:24:54 +0200

    feat(autoload): add autoload system to project

--- public/index.php
+++ public/index.php
@@ -1,5 +1,12 @@
 <?php
 
+
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 session_start();
-$router = new \App\config\Router();
-$router->run();
+// $router = new \App\config\Router();
+// $router->run();
+
+return $this->view->render('index');
