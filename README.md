a0022c0f547299c12deed0a876983aca9b5f62ac
Zeller <matheo.zeller@gmail.com>
2020-08-18 10:10:03 +0200

    git commit -m "fix(codacy): add changes to fix codacy errors"

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 # Blog-Matheo-Zeller
 Hello, I'm a simple front developper. If you come here , that's because you need the bests tips about integration. Also , I'm running PHP/symfony formation. Register to my blog and you will be alter about my news articles.
 
-## Code review and quality :
+## Code review and quality
 [![Codacy Badge](https://api.codacy.com/project/badge/Grade/de7e346a62b24081846796711104ad8d)](https://app.codacy.com/manual/LeZellus/Blog-Matheo-Zeller?utm_source=github.com&utm_medium=referral&utm_content=LeZellus/Blog-Matheo-Zeller&utm_campaign=Badge_Grade_Dashboard)
 
-## Getting started :
+## Getting started
 If you are a developer and you want to install the application on local, you need to follow those steps :
 
-### 1-1 | Create Database :
+### 1-1 | Create Database
 The first step is to create Database, you have 2 ways to do that :
 -   You can go on your local database software and add manually database (on mysql for exemple)
 -   Copy the lines into `sql/STEP0-createDatabase.sql` and paste on your local database software in "SQL" section
--- sql/STEP1-insertOnDatabase.sql
+++ sql/STEP1-insertOnDatabase.sql
@@ -1,48 +1,48 @@
-CREATE TABLE 'article' (
-  'id' int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  'title' varchar(100) NOT NULL,
-  'content' text NOT NULL,
-  'author' varchar(100) NOT NULL,
-  'createdAt' datetime NOT NULL
+CREATE TABLE article (
+  `id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `title` varchar(100) NOT NULL,
+  `content` text NOT NULL,
+  `author` varchar(100) NOT NULL,
+  `createdAt` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE 'user' (
-  'id' int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  'pseudo' varchar(100) NOT NULL,
-  'email' varchar(100) NOT NULL,
-  'firstName' varchar(100) NOT NULL,
-  'lastName' varchar(100) NOT NULL,
-  'password' varchar(60) NOT NULL,
-  'role_id' int(11) NOT NULL,
-  'createdAt' datetime NOT NULL
+CREATE TABLE user (
+  `id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `pseudo` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `firstName` varchar(100) NOT NULL,
+  `lastName` varchar(100) NOT NULL,
+  `password` varchar(60) NOT NULL,
+  `role_id` int(11) NOT NULL,
+  `createdAt` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE 'comment' (
-   'id' int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-   'pseudo' varchar(100) NOT NULL,
-   'content' text NOT NULL,
-   'createdAt' datetime NOT NULL,
-   'flag' BOOLEAN NOT NULL,
-   'article_id' int(11) NOT NULL
+CREATE TABLE comment (
+   `id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+   `pseudo` varchar(100) NOT NULL,
+   `content` text NOT NULL,
+   `createdAt` datetime NOT NULL,
+   `flag` BOOLEAN NOT NULL,
+   `article_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE 'role' (
-  'id' int(11) NOT NULL PRIMARY KEY,
-  'name' varchar(15) NOT NULL
+CREATE TABLE role (
+  `id` int(11) NOT NULL PRIMARY KEY,
+  `name` varchar(15) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO 'role' ('id', 'name') VALUES
-(1, 'admin'),
-(2, 'user');
+INSERT INTO `role` (`id`, `name`) VALUES
+(1, `admin`),
+(2, `user`);
 
-ALTER TABLE 'role'
-  MODIFY 'id' int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+ALTER TABLE `role`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
-ALTER TABLE 'comment'
-  ADD KEY 'fk_article_id' ('article_id');
+ALTER TABLE `comment`
+  ADD KEY `fk_article_id` (`article_id`);
 
-ALTER TABLE 'comment'
-  ADD CONSTRAINT 'fk_article_id' FOREIGN KEY ('article_id') REFERENCES 'article' ('id'); 
+ALTER TABLE `comment`
+  ADD CONSTRAINT `fk_article_id` FOREIGN KEY (`article_id`) REFERENCES `article` (`id`); 
 
-ALTER TABLE 'user'
-  ADD CONSTRAINT 'fk_role_id' FOREIGN KEY ('role_id') REFERENCES 'role' ('id');
+ALTER TABLE `user`
+  ADD CONSTRAINT `fk_role_id` FOREIGN KEY (`role_id`) REFERENCES `role` (`id`);
